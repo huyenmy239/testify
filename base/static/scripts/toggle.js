e21bfd6e892ea1f159d85e9658efe4e44e22e490
Mie <nguyenthihuyenmy239@gmail.com>
2024-07-01 12:52:41 +0700

    Add and edit some icons and images for UI.

--- base/static/scripts/toggle.js
+++ base/static/scripts/toggle.js
@@ -2,17 +2,18 @@ function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
   const main = document.getElementById("main");
   const logo = document.querySelector(".sidebar-logo .logo");
+  const logo_text = document.querySelector(".sidebar-logo .logo-text");
   const links = document.querySelectorAll(".sidebar .menu-a");
 
   if (sidebar.style.width === "80px") {
     sidebar.style.width = "200px";
     main.style.marginLeft = "200px";
-    logo.style.display = "block";
+    logo_text.style.display = "block";
     links.forEach((link) => (link.style.display = "block"));
   } else {
     sidebar.style.width = "80px";
     main.style.marginLeft = "80px";
-    logo.style.display = "block";
+    logo_text.style.display = "none";
     links.forEach((link) => (link.style.display = "none"));
   }
 }
